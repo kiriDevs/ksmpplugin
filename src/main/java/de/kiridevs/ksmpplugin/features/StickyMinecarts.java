22e3e660ff701544953f021c82938c129d4e0080
Kiri <56218513+kiriDevs@users.noreply.github.com>
2024-02-07 00:17:47 +0100

    feat: Add StickyMinecarts
    Making a RideableMinecart sticky with honey will prevent it from moving. Useful for seating (push stair into Minecart). Shears can be used to remove the sticky effect. A stick can be used to rotate a Minecart by 90 degrees to allow players to align them to builds.
    
    Again, the code isn't *great*, but I guess it works. Actually, I'd say
    the code itself isn't the problem, just the structure.

---
+++ src/main/java/de/kiridevs/ksmpplugin/features/StickyMinecarts.java
@@ -0,0 +1,116 @@
+package de.kiridevs.ksmpplugin.features;
+
+import com.destroystokyo.paper.ParticleBuilder;
+import de.kiridevs.ksmpplugin.main.KiriSmpPlugin;
+import io.papermc.paper.event.entity.EntityMoveEvent;
+import net.kyori.adventure.text.Component;
+import org.bukkit.*;
+import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.minecart.RideableMinecart;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.vehicle.VehicleMoveEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.util.Vector;
+
+public class StickyMinecarts implements Listener {
+    private static final String STICKY_MARKER_KEY = "isStickyMinecart";
+    private static final double CART_DEFAULT_MAX_SPEED = 4d;
+
+    private final KiriSmpPlugin plugin;
+    private final NamespacedKey dataKey;
+
+    public StickyMinecarts(KiriSmpPlugin plugin) {
+        this.plugin = plugin;
+        this.dataKey = new NamespacedKey(this.plugin, STICKY_MARKER_KEY);
+    }
+
+    private boolean makeSticky(RideableMinecart cart) {
+        PersistentDataContainer pdc = cart.getPersistentDataContainer();
+        if (pdc.getOrDefault(dataKey, PersistentDataType.BOOLEAN, false)) {
+            // Cart is already sticky
+            return false;
+        }
+        pdc.set(this.dataKey, PersistentDataType.BOOLEAN, true);
+
+        // Stop cart from moving
+        cart.setVelocity(new Vector(0, 0, 0));
+        cart.setMaxSpeed(0);
+
+        // Play waxing effect
+        Location cartLoc = cart.getLocation();
+        cartLoc.getWorld().playEffect(cartLoc, Effect.COPPER_WAX_ON, 0);
+
+        return true;
+    }
+
+    private boolean unsticky(RideableMinecart cart) {
+        // Remove flag
+        PersistentDataContainer pdc = cart.getPersistentDataContainer();
+        boolean sticky = pdc.getOrDefault(this.dataKey, PersistentDataType.BOOLEAN, false);
+        if (!sticky) return false;
+
+        cart.setMaxSpeed(CART_DEFAULT_MAX_SPEED);
+        pdc.remove(dataKey);
+
+        // Play effect
+        Location cartLoc = cart.getLocation();
+        cartLoc.getWorld().playEffect(cartLoc, Effect.COPPER_WAX_OFF, 0);
+
+        return true;
+    }
+
+    @EventHandler
+    public void onPlayerInteractEvent(PlayerInteractEntityEvent event) {
+        if (!(event.getRightClicked() instanceof RideableMinecart cart)) return;
+
+        Player player = event.getPlayer();
+        PlayerInventory inv = player.getInventory();
+        ItemStack main = inv.getItemInMainHand();
+
+        event.setCancelled(true); // Prevent sit-down
+        switch (main.getType()) {
+            case HONEY_BOTTLE -> {
+                boolean stickied = this.makeSticky(cart);
+                // Remove glue from inventory
+                if (!stickied) return;
+                main.subtract(1);
+                inv.addItem(new ItemStack(Material.GLASS_BOTTLE, 1));
+            }
+            case SHEARS -> {
+                boolean unstickied = this.unsticky(cart);
+                // Remove 1 durability from the used shears
+                if (!unstickied) return;
+                Damageable dmgShears = (Damageable) main.getItemMeta();
+                dmgShears.setDamage(dmgShears.getDamage() + 1);
+                main.setItemMeta(dmgShears);
+            }
+            case STICK -> cart.setRotation(90, 0);
+            default -> event.setCancelled(false); // Allow sitdown without special item
+        }
+    }
+
+    @EventHandler
+    public void onMinecartMove(VehicleMoveEvent event) {
+        if (!(event.getVehicle() instanceof RideableMinecart cart)) return;
+        if (
+            !cart.getPersistentDataContainer()
+            .getOrDefault(this.dataKey, PersistentDataType.BOOLEAN, false)
+        ) return;
+
+        double yVelo = cart.getVelocity().getY();
+        cart.setVelocity(new Vector(0, yVelo, 0));
+        cart.setMaxSpeed(0);
+    }
+
+    public void init() {
+        Bukkit.getPluginManager().registerEvents(this, this.plugin);
+    }
+}

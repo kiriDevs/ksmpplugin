fa3223d27dd7923eee3e31568804f1db7426bf3e
Kiri <56218513+kiriDevs@users.noreply.github.com>
2024-02-12 11:10:10 +0100

    refac: Refactor SaddleRecipe

--- src/main/java/de/kiridevs/ksmpplugin/recipes/CraftingRecipes.java
+++ src/main/java/de/kiridevs/ksmpplugin/recipes/CraftingRecipes.java
@@ -17,12 +17,11 @@ public final class CraftingRecipes {
         new BellRecipe(plugin).register(config);
         new BundleRecipe(plugin).register(config);
         new ChainRecipes(plugin).register(config);
+        new SaddleRecipe(plugin).register(config);
 
         // Copied from KiriSmpPlugin.java
         if (!config.getString("chainArmor").equals("false")) {
             new ChainArmorRecipes(plugin, config.getString("chainArmor")).register();
         }
-
-        if (config.getBoolean("saddle")) new SaddleRecipe(plugin).register();
     }
 }

6782f2dffe519ac215d77ab3c779d148ea18e19d
Kiri <56218513+kiriDevs@users.noreply.github.com>
2024-06-21 10:43:45 +0200

    feat: Add Shroomlight crafting

---
+++ src/main/java/de/kiridevs/ksmpplugin/recipes/crafting/ShroomlightRecipe.java
@@ -0,0 +1,39 @@
+package de.kiridevs.ksmpplugin.recipes.crafting;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.RecipeChoice.MaterialChoice;
+
+import de.kiridevs.ksmpplugin.main.KiriSmpPlugin;
+
+public class ShroomlightRecipe {
+    private final KiriSmpPlugin plugin;
+
+    private static final MaterialChoice SHROOM_BLOCKS = new MaterialChoice(
+            Material.BROWN_MUSHROOM_BLOCK,
+            Material.RED_MUSHROOM_BLOCK
+    );
+
+    public ShroomlightRecipe(KiriSmpPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void register(ConfigurationSection config) {
+        if (!config.getBoolean("shroomlight")) return;
+
+        NamespacedKey key = new NamespacedKey(this.plugin, "shroomlight");
+        ItemStack result = new ItemStack(Material.SHROOMLIGHT);
+
+        ShapedRecipe recipe = new ShapedRecipe(key, result)
+            .shape(" g", "gsg", " g")
+            .setIngredient('g', Material.GLOWSTONE_DUST)
+            .setIngredient('s', ShroomlightRecipe.SHROOM_BLOCKS);
+
+        this.plugin.log.info("recipes: ShroomlightRecipe: Registering Custom Recipe");
+        Bukkit.addRecipe(recipe);
+    }
+}
